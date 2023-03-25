be95074df80922726c7b943b79afff362fcdce54
Son Vu <son.vu@smartbear.com>
2023-03-25 19:29:36 +0700

    update

--- kafka-websocket-consumer/src/main/java/son/vu/websocket/config/WebSocketConfig.java
+++ kafka-websocket-consumer/src/main/java/son/vu/websocket/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package son.vu.websocket.config;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.modelmapper.ModelMapper;
@@ -11,6 +12,8 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -56,12 +59,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         return new ModelMapper();
     }
 
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+
         registry.addEndpoint("/socket")
-                .setAllowedOrigins("http://localhost:4200", "http://localhost:4200"
-                        , "http://websocket:7070", "http://websocket:7070"
-                        , "http://keycloak:7070", "http://keycloak:7070")
+                .setAllowedOrigins("http://localhost:4200", "http://websocket:4200"
+                , "http://localhost:7070", "http://websocket:7070"
+                        , "http://localhost:6060", "http://websocket:6060"
+                , "http://localhost:7070", "http://keycloak:7070")
                 .withSockJS();
     }
 
