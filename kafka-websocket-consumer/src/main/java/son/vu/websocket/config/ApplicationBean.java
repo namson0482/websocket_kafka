764a8725d5511df437c02c9ecd3042656444cd3f
namson0482 <namson0482@gmail.com>
2023-03-26 20:00:40 +0700

    Feature/schema avro (#2)
    * update
    
    * update
    
    * update
    
    * Angular update to latest version 15
    
    * Angular update to latest version 15
    
    * Angular update to latest version 15
    
    * Angular update to latest version 15
    
    * Angular update to latest version 15
    
    * Angular update to latest version 15
    
    * Angular update to latest version 15
    
    * Angular update to latest version 15
    
    * Angular update to latest version 15
    
    * Angular update to latest version 15
    
    * Give readme to build all images and run
    
    * change to property format
    
    * change to property format
    
    * init data once web load
    
    ---------
    
    Co-authored-by: Son Vu <son.vu@smartbear.com>

---
+++ kafka-websocket-consumer/src/main/java/son/vu/websocket/config/ApplicationBean.java
@@ -0,0 +1,14 @@
+package son.vu.websocket.config;
+
+
+import lombok.Data;
+
+@Data
+public class ApplicationBean {
+
+    public ApplicationBean() {
+
+    }
+    private String data;
+
+}
--- kafka-websocket-consumer/src/main/java/son/vu/websocket/config/WebSocketConfig.java
+++ kafka-websocket-consumer/src/main/java/son/vu/websocket/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package son.vu.websocket.config;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.modelmapper.ModelMapper;
@@ -12,8 +11,6 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -59,13 +56,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         return new ModelMapper();
     }
 
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
         registry.addEndpoint("/socket")
                 .setAllowedOrigins("http://localhost:4200", "http://websocket:4200"
-                , "http://localhost:7070", "http://websocket:7070"
                         , "http://localhost:6060", "http://websocket:6060"
                 , "http://localhost:7070", "http://keycloak:7070")
                 .withSockJS();
