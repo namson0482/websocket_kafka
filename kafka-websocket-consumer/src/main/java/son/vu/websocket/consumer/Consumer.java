3b0019929a8c1d638174422e34f18440654da080
Son Vu <son.vu@smartbear.com>
2023-03-31 20:37:10 +0700

    implementing new source code with avro schema

--- kafka-websocket-consumer/src/main/java/son/vu/websocket/consumer/Consumer.java
+++ kafka-websocket-consumer/src/main/java/son/vu/websocket/consumer/Consumer.java
@@ -1,30 +1,19 @@
 package son.vu.websocket.consumer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.opencsv.bean.ColumnPositionMappingStrategy;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import lombok.Data;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import son.vu.avro.domain.Customer;
-import son.vu.avro.domain.SaleDetail;
-import son.vu.avro.domain.SaleDetailRecord;
-import son.vu.avro.domain.SaleReport;
-import son.vu.websocket.config.ApplicationBean;
-import son.vu.websocket.controller.WebSocketController;
-import son.vu.websocket.domain.Message;
-import son.vu.websocket.domain.dto.MessageDto;
-import son.vu.websocket.service.MessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import son.vu.avro.domain.SaleReport;
+import son.vu.websocket.config.ApplicationBean;
+import son.vu.websocket.controller.WebSocketController;
+import son.vu.websocket.service.MessageService;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.HashMap;
@@ -54,64 +43,11 @@ public class Consumer {
         this.webSocketController = webSocketController;
     }
 
-    private String proceedMessage(String[] arrayValues) {
-
-        HashMap<String, Product> map = new HashMap();
-        for(int i=1;i<arrayValues.length;i++) {
-            String []items = arrayValues[i].split("\\|");
-            String key = items[2].toLowerCase();
-            if(map.containsKey(key)) {
-                Product product = map.get(key) ;
-                product.setQuantity(product.getQuantity() + Integer.parseInt(items[3]));
-
-                String tempMoney = items[4].replaceAll("[\\.\\D]", "");
-                BigDecimal temp = product.getMoney();
-                temp.add(new BigDecimal(Integer.parseInt(tempMoney)));
-            } else {
-                Product product = new Product();
-                product.setName(items[2]);
-                product.setQuantity(Integer.parseInt(items[3]));
-                String tempMoney = items[4].replaceAll("[\\.\\D]", "");
-                BigDecimal money = new BigDecimal(Integer.parseInt(tempMoney));
-                product.setMoney(money);
-                map.put(items[2].toLowerCase(), product);
-            }
-        }
-
-        String result = "";
-
-        for (Map.Entry<String, Product> entry : map.entrySet()) {
-            String key = entry.getKey();
-            Product product = entry.getValue();
-            String line = key + "$" + product.getQuantity() + "$" + product.getMoney().toString();
-            if(result.equals("")) {
-                result += line;
-            } else {
-                result += "@" + line ;
-            }
-        }
-
-        return result;
-    }
-
     @KafkaListener(topics = orderTopic)
     public void consumeMessage(SaleReport saleReport) throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
-        FileWriter writer = new FileWriter("");
 
-        var mappingStrategy =  new ColumnPositionMappingStrategy();
-
-        mappingStrategy.setType(SaleDetailRecord.class);
-        String[] columns = new String[]
-                { "SalesDate", "StoreName", "ProductName", "SalesUnits", "SalesRevenue" };
-        mappingStrategy.setColumnMapping(columns);
-        var builder=  new StatefulBeanToCsvBuilder(writer);
-
-        StatefulBeanToCsv beanWriter =
-                builder.withMappingStrategy(mappingStrategy).build();
-
-        // Write list to StatefulBeanToCsv object
-        beanWriter.write(saleReport.getSaleDetailList());
-        log.info(saleReport.toString());
+        messageService.persistMessage(saleReport);
+        log.info(TOTAL_ITEM_RECEIVED);
     }
 
 }
--- kafka-websocket-consumer/src/main/java/son/vu/websocket/service/MessageService.java
+++ kafka-websocket-consumer/src/main/java/son/vu/websocket/service/MessageService.java
@@ -1,12 +1,18 @@
 package son.vu.websocket.service;
 
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import son.vu.avro.domain.SaleDetailRecord;
+import son.vu.avro.domain.SaleReport;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 
 @Slf4j
@@ -19,22 +25,28 @@ public class MessageService {
 
     private static final String HEADER_FILE = "\"SalesDate\"|\"StoreName\"|\"ProductName\"|\"SalesUnits\"|\"SalesRevenue\"";
 
-    public void persistMessage(String []arrayValues) {
+    public void persistMessage(SaleReport saleReport) throws CsvRequiredFieldEmptyException, CsvDataTypeMismatchException, IOException {
 
         Date date = new Date();
         long timeMilli = date.getTime();
         String fileName = dataFilePath + "/" + "Sales_" + timeMilli + ".psv";
 
-        try (PrintWriter writer = new PrintWriter(fileName)) {
-            writer.write(HEADER_FILE + "\n");
-            for(String item: arrayValues) {
-                writer.write(item + "\n");
-            }
-            writer.close();
-            log.info("Write file done!");
-        } catch (FileNotFoundException e) {
-            log.error(e.getMessage());
-        }
+        FileWriter writer = new FileWriter(fileName);
+
+        var mappingStrategy =  new ColumnPositionMappingStrategy();
+
+        mappingStrategy.setType(SaleDetailRecord.class);
+        String[] columns = new String[]
+                { "SalesDate", "StoreName", "ProductName", "SalesUnits", "SalesRevenue" };
+        mappingStrategy.setColumnMapping(columns);
+        var builder=  new StatefulBeanToCsvBuilder(writer);
+
+        StatefulBeanToCsv beanWriter =
+                builder.withMappingStrategy(mappingStrategy).build();
+
+        // Write list to StatefulBeanToCsv object
+        beanWriter.write(saleReport.getSaleDetailList());
+
 
     }
 
